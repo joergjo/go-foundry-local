3b3d732ed46787ccc90cb8ab20ed2e9a777e2f38
Jörg Jooss <joerg@joergjooss.de>
2025-12-24 10:38:45 +0100

    wip: add roundtripper to log HTTP response
    Signed-off-by: Jörg Jooss <joerg@joergjooss.de>

--- examples/chat-completion-streaming/main.go
+++ examples/chat-completion-streaming/main.go
@@ -6,12 +6,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/joergjo/go-foundry-local/foundrylocal"
 	"github.com/tmaxmax/go-sse"
 )
+
+type loggingTransport struct {
+	innerTransport http.RoundTripper
+	file           *os.File
+}
+
+func (t *loggingTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	bytes, _ := httputil.DumpRequestOut(r, true)
+	resp, err := t.innerTransport.RoundTrip(r)
+	respBytes, _ := httputil.DumpResponse(resp, true)
+	bytes = append(bytes, respBytes...)
+	t.file.Write(bytes)
+	return resp, err
+}
 
 // chunk represents an OpenAI chat completion chunk, reduced to only those properties
 // required for this demo. See https://platform.openai.com/docs/api-reference/chat-streaming.
@@ -36,6 +52,20 @@ type completionRequest struct {
 }
 
 func main() {
+	timestamp := time.Now().Format("2006-01-02T15-04-05.000")
+	logFileName := fmt.Sprintf("httpdump-%s.log", timestamp)
+	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open log file: %v", err))
+	}
+	defer logFile.Close()
+
+	lt := &loggingTransport{
+		innerTransport: http.DefaultTransport,
+		file:           logFile,
+	}
+	http.DefaultTransport = lt
+
 	alias := "qwen2.5-1.5b"
 
 	// Start the Foundry Local service with the specified model alias.
